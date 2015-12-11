9539d5a5128bb29321b05f25608344f87bd0b059
SuperJedi224 <superjedi224@gmail.com>
2015-12-11 13:57:38 -0500

    Updated HOH cutoff dates

--- bot.js
+++ bot.js
@@ -71,7 +71,8 @@ post(Math.log10(n(k.substring(4))),user);
 }
 if(k.indexOf("hoh_")==0){
 var i=k.substring(4);
-var char="Tobi";
+var char="Burk";
+if(i<505)char="Tobi";
 if(i<491)char="Noah";
 if(i<472)char="Burk";
 if(i<459)char="Tobi";
